b0fbb9c11364ccd78ecf6a7ea13cb121285c094c
akelc <akelcheg@gmail.com>
2016-12-25 19:41:15 +0200

    tt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,18 @@
 @endpush
 
 @section('content')
+
+    <section class="intro">
+        <div class="section-content">
+            <h1 class="heading">TRY TO FIND OUT FOR 60 MINUTES</h1>
+            <input type="button" class="quest-button" value="CHOSE YOUR MISSION"/>
+        </div>
+    </section>
+
     <section class="white">
         <div class="section-content">
             <p><i class="fa fa-key" aria-hidden="true"></i></p>
-            <p class="heading">THE ROOMS</p>
+            <h2 class="heading">THE ROOMS</h2>
         </div>
     </section>
 @endsection
