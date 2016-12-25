1428c054348a25ffcbce77ad6790316f2f8a0cf6
akelc <akelcheg@gmail.com>
2016-12-25 17:46:43 +0200

    tt

--- resources/assets/js/basic.js
+++
@@ -1,9 +0,0 @@
-function setNav() {
-    alert("dsa");
-    var x = document.getElementById("navigation");
-    if (x.className === "nav-menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-menu";
-    }
-}
