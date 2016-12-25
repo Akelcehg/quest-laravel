a0584fe8afb7a20cb835e41d6df8c8220f9c298a
akelc <akelcheg@gmail.com>
2016-12-25 19:25:28 +0200

    tt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
-        123
+    <section class="white">
+        <div class="section-content">
+            <p><i class="fa fa-key" aria-hidden="true"></i></p>
+            <p class="heading">THE ROOMS</p>
+        </div>
     </section>
 @endsection
