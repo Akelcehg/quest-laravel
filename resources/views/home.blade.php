266027d00c4e55be5f709d0f350a0fcbe9ef5317
akelc <akelcheg@gmail.com>
2016-12-25 21:32:12 +0200

    tt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @push('styles')
-    <link href="/css/home.css" rel="stylesheet">
+<link href="/css/home.css" rel="stylesheet">
 @endpush
 
 @section('content')
@@ -10,6 +10,51 @@
         <div class="section-content">
             <h1 class="heading">TRY TO FIND OUT FOR 60 MINUTES</h1>
             <input type="button" class="quest-button" value="CHOSE YOUR MISSION"/>
+        </div>
+    </section>
+
+    <section class="tasks">
+        <div class="section-content">
+            <div class="left-side">
+                <div class="task-container">
+                    <p class="heading text-right">TASK 01</p>
+
+                    <p class="desc text-right l">Logical problems and puzzles. No physical effort . Only wit and
+                        observation.
+                        Everything is in
+                        your head. </p>
+
+                </div>
+                <div class="task-container">
+                    <p class="heading text-right">TASK 02</p>
+                    <p class="desc text-right l">Logical problems and puzzles. No physical effort . Only wit and
+                        observation.
+                        Everything is in
+                        your head. </p>
+                </div>
+            </div>
+
+            <div class="task-center mid">
+                <img class="key" src="img/key_b.png">
+            </div>
+            <div class="right-side">
+                <div class="task-container">
+                    <p class="heading text-left">TASK 03</p>
+                    <p class="desc text-left r">Logical problems and puzzles. No physical effort . Only wit and
+                        observation.
+                        Everything is in
+                        your
+                        head. </p>
+
+                </div>
+                <div class="task-container">
+                    <p class="heading text-left">TASK 04</p>
+                    <p class="desc text-left r">Logical problems and puzzles. No physical effort . Only wit and
+                        observation.
+                        Everything is in
+                        your head. </p>
+                </div>
+            </div>
         </div>
     </section>
 
