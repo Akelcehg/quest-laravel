15ae6ecaa378ec1fb1757250785a5aeaf843ad13
akelc <akelcheg@gmail.com>
2016-12-30 00:27:17 +0200

    tt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -160,12 +160,12 @@
                         </div>
 
                     </div>
-                    <img src="/img/quests/1.jpg">
+                    <img src="/img/quests/2.jpg">
                 </div>
             </div>
-            <div class="quest-half"><img src="/img/quests/1.jpg"></div>
+            <div class="quest-half"><img src="/img/quests/2.jpg"></div>
 
-            <div class="quest-half"><img src="/img/quests/1.jpg"></div>
+            <div class="quest-half"><img src="/img/quests/3.jpg"></div>
             <div class="quest-half quest-desc">
                 <div class="wrap">
                     <div class="layer"></div>
@@ -195,78 +195,11 @@
                         </div>
 
                     </div>
-                    <img src="/img/quests/1.jpg">
+                    <img src="/img/quests/3.jpg">
                 </div>
             </div>
 
-
-            <div class="quest-half quest-desc">
-                <div class="wrap">
-                    <div class="layer"></div>
-
-                    <div class="text">
-
-                        <div class="info">
-                            <h3>MODER ART MUSEUM</h3>
-                            <p>Lorem ipsum dolor sit amet, idque deserunt indoctum no mei, ius ea solum vocibus
-                                platonem, has mazim appetere eu. Pri delenit minimum legendos in, in cum lorem lucilius
-                                hendrerit. Cibo assum mucius cu eum. Minim mediocrem ea qui, vix et enim meliore.</p>
-                            <div class="additional-info">
-                                <p>Wall sterrt</p>
-                                <p>2 - 4 people</p>
-                                <p>60 min</p>
-                            </div>
-
-                            <div class="button-wrap">
-                                <a href="#" class="quest-button">ПОДРОБНЕЕ</a>
-                            </div>
-
-                        </div>
-                        <div class="price">
-                            <div class="price-circle">
-                                <p class="price-value">666 грн.</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <img src="/img/quests/1.jpg">
-                </div>
-            </div>
-            <div class="quest-half"><img src="/img/quests/1.jpg"></div>
         </div>
-
-        {{--<div class="quest-half"><img src="/img/quests/1.jpg"></div>
-        <div class="quest-half quest-desc">
-            <div class="wrap">
-                <div class="layer"></div>
-
-                <div class="text">
-
-                    <div class="info">
-                        <h3>MODER ART MUSEUM</h3>
-                        <p>Lorem ipsum dolor sit amet, idque deserunt indoctum no mei, ius ea solum vocibus
-                            platonem, has mazim appetere eu. Pri delenit minimum legendos in, in cum lorem lucilius
-                            hendrerit. Cibo assum mucius cu eum. Minim mediocrem ea qui, vix et enim meliore.</p>
-
-                        <p>Wall sterrt</p>
-                        <p>2 - 4 people</p>
-                        <p>60 min</p>
-                    </div>
-                    <div class="price">
-                        <p>666</p>
-                    </div>
-
-                </div>
-                <img src="/img/quests/1.jpg">
-            </div>
-        </div>--}}
-
-        {{--<div class="quest-half">2</div>
-        <div class="quest-half"><img src="/img/quests/2.jpg"></div>
-
-        <div class="quest-half"><img src="/img/quests/3.jpg"></div>
-        <div class="quest-half">3</div>--}}
-
 
     </section>
 
